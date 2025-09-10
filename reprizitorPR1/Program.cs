4dd19acba1e86176f1e15c67121fe6c07c422f45
ИСП-44 <-44@LAB25-11>
2025-09-10 12:03:44 +0300

    Добавьте файлы проекта.

---
+++ reprizitorPR1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace reprizitorPR1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("строки с преобладанием положительных элементов ");
+
+            Console.Write("Введите количество строк (n): ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов (m): ");
+            int m = int.Parse(Console.ReadLine());
+
+            
+            double[,] matrix = new double[n, m];
+            Random rand = new Random();
+
+            // Автоматически заполняем матрицу 
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    // Генерируем случайное целое число от -9 до 9
+                    matrix[i, j] = rand.Next(-9, 10); 
+                }
+            }
+
+            // Выводим сгенерированную матрицу для наглядности
+            Console.WriteLine("\nСгенерированная матрица:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{matrix[i, j],4} "); // 4 символа на число для выравнивания
+                }
+                Console.WriteLine(); // Переход на новую строку 
+            }
+
+            Console.WriteLine("\n=== Результат анализа ===");
+            int strociCount = 0; // Счётчик строк
+            // Проходимся по матрице
+            for (int i = 0; i < n; i++)
+            {
+                int polCount = 0;     
+                int otrizCount = 0;  
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i, j] > 0)
+                        polCount++;
+                    else if (matrix[i, j] < 0)
+                        otrizCount++;
+                }
+                // положительных элементов больше чем отрицательных
+                if (polCount > otrizCount)
+                {
+                    Console.WriteLine($"Строка {i + 1} (индекс {i}): положительных = {polCount}, отрицательных = {otrizCount}");
+                    strociCount++; 
+                }
+            }
+            if (strociCount == 0)
+            {
+                Console.WriteLine("Нет строк, где положительных элементов больше, чем отрицательных.");
+            }
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
